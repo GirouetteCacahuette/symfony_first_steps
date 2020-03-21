80fe420d453cf7577999df86289464d47a6d9629
Martin Prieur <martin.prieur@viseo.com>
2020-03-21 23:17:05 +0100

    more progress"

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -25,6 +26,15 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show question "%s" lama', $slug));
+        $answers = [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3"
+        ];
+
+        return $this->render("/question/show.html.twig", [
+           "question" => strtoupper($slug),
+            "answers" => $answers
+        ]);
     }
 }
